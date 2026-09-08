f21083ed87518245df0de5f1d834ec256211b0eb
zhengshangui <80737250@qq.com>
2026-09-08 12:55:59 +0800

    Update 01_prepare.sh

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -155,12 +155,15 @@ fi
 exit 0
 EOF
 chmod +x files/etc/uci-defaults/99_fake_ruijie
-# 内核模块，25.12必须编译内置
-CONFIG_PACKAGE_kmod-tun=y
-CONFIG_PACKAGE_kmod-inet-diag=y
-CONFIG_PACKAGE_kmod-nft-tproxy=y
-CONFIG_PACKAGE_libatomic=y
-CONFIG_PACKAGE_libstdcpp=y
-# 把默认ip‑tiny替换成完整版ip‑full
-CONFIG_PACKAGE_ip-full=y
-CONFIG_PACKAGE_ip-tiny=n
+ # ====================新增：写入25.12必需内核模块配置====================
+ cat >> .config <<'EOF'
+ # 内核模块，25.12必须编译内置
+ CONFIG_PACKAGE_kmod-tun=y
+ CONFIG_PACKAGE_kmod-inet-diag=y
+ CONFIG_PACKAGE_kmod-nft-tproxy=y
+ CONFIG_PACKAGE_libatomic=y
+ CONFIG_PACKAGE_libstdcpp=y
+ # 把默认ip‑tiny替换成完整版ip‑full
+ CONFIG_PACKAGE_ip-full=y
+ CONFIG_PACKAGE_ip-tiny=n
+ EOF
